0820417abc4be0e044103266337904076b820dbe
Leo <tieuvu4803@gmail.com>
2024-05-06 01:03:36 +0700

    init

---
+++ BookShop/Models/ApplicationModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookShop.Models
+{
+    public class ApplicationModel
+    {
+        [Key]
+        public string ApplicationId { get; set; }
+
+        [Required]
+        public string JobListingId { get; set; }
+
+        [Required]
+        public string? Message { get; set; }
+
+        // Navigation property if needed
+        public string? Description { get; set; }
+
+        public bool? status { get; set; }
+
+    }
+}
